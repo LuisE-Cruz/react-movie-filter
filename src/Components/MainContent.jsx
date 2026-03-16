9518f3b4f9547df274df333ab63fb05d2eeb9227
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-03-16 12:12:00 +0100

    Aggiunta lista film con map su MainContent, styling della lista

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react"
+import films from "../Data/films"
+
+
 export default function MainContent() {
+
+    const [filmList, setFilmList] = useState(films)
+
+
     return (
         <>
+            <div className="container-list">
+                <ul>
+                    {filmList.map((film, index) => (
+                        <li className="list-film" key={index}>
+                            <a className="list-link" href="#">
+                                {film.title} <br />
+                                {film.genre}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     )
 }
